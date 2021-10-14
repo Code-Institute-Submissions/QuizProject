fc04c7472a45947d934de3feb3c0e1a559e5aaff
ericmcg96 <ericmcg4@gmail.com>
2021-10-14 18:59:43 +0000

    get elements from the DOM.

---
+++ Assets/main.js
@@ -0,0 +1,7 @@
+let incorrectmessage = document.getElementById('incorrect-message');
+let correctmessage = document.getElementById('correct-message');
+let score = document.getElementById('score');
+let answered = document.getElementById('answered');
+let index = document.getElementById('main-body');
+let endMessage = document.getElementById('end-message');
+let endScore = document.getElementById('end-score');
