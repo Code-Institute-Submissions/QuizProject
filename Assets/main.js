2ff143e95843b65f151c09fc4d4f7ccc9e89d20c
ericmcg96 <ericmcg4@gmail.com>
2021-10-14 21:07:42 +0000

    add question content and css

--- Assets/main.js
+++ Assets/main.js
@@ -5,3 +5,59 @@ let answered = document.getElementById('answered');
 let index = document.getElementById('main-body');
 let endMessage = document.getElementById('end-message');
 let endScore = document.getElementById('end-score');
+endScore.value = endScore.innerHTML;
+endScore.innerHTML = 0;
+
+document.querySelectorAll('.correct').forEach(item => { 
+    item.addEventListener('click', function(event) {
+        correctmessage.innerHTML = 'Correct!';
+        incorrectmessage.style.display = 'none'
+        correctmessage.style.display = 'inline-block';
+        score.innerHTML++;
+        endScore.innerHTML++;
+        answered.innerHTML++;
+
+        if (answered.innerHTML ==  1) {
+            index.style.display = 'none';
+            endMessage.style.display = 'inline-block';
+            
+        }
+
+        if(true) {
+            setTimeout( function() {
+                correctmessage.style.display = 'none';
+            }, 1000) 
+        }
+
+        
+    })
+  
+});
+
+document.querySelectorAll('.wrong').forEach(item => { 
+    item.addEventListener('click', function(event) {
+        incorrectmessage.innerHTML = 'Wrong Answer!';
+        incorrectmessage.style.display = 'inline-block';
+        correctmessage.style.display = 'none';
+        answered.innerHTML++;
+      
+
+        if (answered.innerHTML ==  1) {
+            index.style.display = 'none';
+            endMessage.style.display = 'inline-block';
+            answered.innerHTML++;
+
+           
+        }
+
+        if(true) {
+            setTimeout( function() {
+                incorrectmessage.style.display = 'none';
+            }, 1000) 
+        }
+
+        
+
+    })
+   
+});
